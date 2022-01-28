a7111cee0d48a6ab18c7715c57ba13cb4a33cc76
Emerson Junqueira <emoonlit@gmail.com>
2022-01-28 14:56:05 -0300

    refactor: add heroku correct endpoint

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import React, { useEffect, useState } from 'react';
 
-const API_ENDPOINT = 'http://localhost:3000/users';
+const API_ENDPOINT = 'https://back-test-app-2502.herokuapp.com/users';
 
 function App() {
   const [data, setData] = useState([]);
