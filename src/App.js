12b189c86e3a091a0ad7f0b319345f7c7b42c632
MuriloM <muriloommaia@gmail.com>
2022-02-10 17:49:25 -0300

    adiciona endpoint

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import React, { useEffect, useState } from 'react';
 
-const API_ENDPOINT = 'http://localhost:3000/users';
+const API_ENDPOINT = 'https://backend-exercise-trybe.herokuapp.com/users';
 
 function App() {
   const [data, setData] = useState([]);
