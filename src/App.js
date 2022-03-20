c7646af606a418446afea08f0a83cc35b06d12b3
Bruno Bartolomeu <bruno.cbartlomeu@gmail.com>
2022-03-20 12:04:12 -0300

    deploy heroku

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import React, { useEffect, useState } from 'react';
 
-const API_ENDPOINT = 'http://localhost:3000/users';
+const API_ENDPOINT = 'https://wemenarentwebackend.herokuapp.com/users';
 
 function App() {
   const [data, setData] = useState([]);
