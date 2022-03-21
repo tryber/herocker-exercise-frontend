9948300695f8139bb369df60088a109e3d057371
Carlos Afonso <carlos.flach15@gmail.com>
2022-03-21 18:15:35 -0300

    Conecta aplicaçao React a API do Supabase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import React, { useEffect, useState } from 'react';
 
-const API_ENDPOINT = 'http://localhost:3000/users';
+const API_ENDPOINT = 'URL_API_BACKEND_HEROKU/users';
 
 function App() {
   const [data, setData] = useState([]);
