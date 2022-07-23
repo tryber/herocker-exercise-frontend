8089cfb49ebe20c20887a8b84a9d9e92232f4294
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-07-23 19:37:26 -0300

    add 3 points

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       .then((parsedData) => { setData(parsedData); setLoading(false); });
   }, []);
 
-  if (loading) return <h1>loading</h1>;
+  if (loading) return <h1>loading...</h1>;
 
   return (
     <table>
