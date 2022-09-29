865f3daeffb7059063191a6cf2f553109ddb8d63
GiovannaMorais <gigimorais9@hotmail.com>
2022-09-29 11:43:18 -0300

    Conecta aplicação React à Api do Supabase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import React, { useEffect, useState } from 'react';
 
-const API_ENDPOINT = 'URL_API_BACKEND_HEROKU/users';
+const API_ENDPOINT = 'https://giovanna-morais.herokuapp.com/users';
 
 function App() {
   const [data, setData] = useState([]);
